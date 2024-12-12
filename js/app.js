414db4d673ef59e6e6423ff6cfaf8198910fc8e5
James Palumbo <jamesmpalumbo1@gmail.com>
2024-12-11 19:25:41 -0500

    added game logic

--- js/app.js
+++ js/app.js
@@ -4,36 +4,68 @@ const choices = ["red", "green", "blue", "yellow"];
 
 /*-------------------------------- Variables --------------------------------*/
 
-let pattern = [];
 let round = 1;
 let playerPattern = [];
 let computerPattern = [];
-let newPlayerPattern = [];
-let newComputerPattern = [];
 let msg = "";
 
 /*------------------------ Cached Element References ------------------------*/
 const messageEl = document.querySelector("#message");
 const highScoreEl = document.querySelector("#high-score");
+const buttons = document.querySelectorAll(".game-button");
 /*-------------------------------- Functions --------------------------------*/
 
-const getPlayerPattern = (event) => {
-  playerPattern.push(event.target.id);
-  newPlayerPattern = [...playerPattern];
+const flashButton = (choice) => {
+  const button = document.getElementById(choice);
+  button.classList.add("active");
+  setTimeout(() => {
+    button.classList.remove("active");
+  }, 500);
+};
+
+const flashPattern = () => {
+  computerPattern.forEach((choice, index) => {
+    setTimeout(() => {
+      flashButton(choice);
+    }, 800 * index);
+  });
 };
 
 const getComputerPattern = () => {
   const randomIndex = Math.floor(Math.random() * choices.length);
   computerPattern.push(choices[randomIndex]);
-  newComputerPattern = [...computerPattern];
+};
+
+const nextRound = () => {
+  playerPattern = [];
+  round += 1;
+  msg = `round: ${round}`;
+  render();
+  getComputerPattern();
+  flashPattern();
 };
 
 const comparePatterns = () => {
-  if (newPlayerPattern.join(",") === newComputerPattern.join(",")) {
-    // nextRound();
-    msg = "you did it!";
-  } else if (newPlayerPattern.join(",") !== newComputerPattern.join(",")) {
-    msg = `Oops wrong color! You final score: ${round}`;
+  const currentChoice = playerPattern.length - 1;
+
+  if (playerPattern[currentChoice] !== computerPattern[currentChoice]) {
+    msg = `Oops wrong color! Your final score: ${round}`;
+    render();
+    return false;
+  }
+
+  if (playerPattern.length === computerPattern.length) {
+    setTimeout(nextRound, 1000);
+    return true;
+  }
+};
+
+const getPlayerPattern = (event) => {
+  const choice = event.target.id;
+  flashButton(choice);
+  playerPattern.push(choice);
+  if (comparePatterns() === false) {
+    return;
   }
 };
 
@@ -41,30 +73,25 @@ const render = () => {
   messageEl.innerText = msg;
 };
 
-const play = (event) => {
-  getPlayerPattern(event);
-  getComputerPattern();
-  comparePatterns();
-  console.log("P:", newPlayerPattern);
-  console.log("C:", newComputerPattern);
-  render();
-};
-
-// const nextRound = () => {};
-
 const reset = () => {
-  pattern = [];
   round = 1;
   playerPattern = [];
   computerPattern = [];
-  newPlayerPattern = [];
-  newComputerPattern = [];
-  msg = "";
-  messageEl.innerText = `round: ${round}`;
+  msg = `Round: ${round}`;
+  render();
+  getComputerPattern();
+  flashPattern();
 };
+
 /*----------------------------- Event Listeners -----------------------------*/
-document.querySelector("#red").addEventListener("click", play);
-document.querySelector("#green").addEventListener("click", play);
-document.querySelector("#blue").addEventListener("click", play);
-document.querySelector("#yellow").addEventListener("click", play);
+buttons.forEach((button) => {
+  button.addEventListener("click", getPlayerPattern);
+});
+
 document.querySelector("#reset-button").addEventListener("click", reset);
+
+//one way to add logic?????
+// 1.create a loop that runs through computerPattern Array
+// 2.change css of buttons for player to mimic
+// 3. set a delay after computer pattern to establish
+// players by possibly using setTimeout()
